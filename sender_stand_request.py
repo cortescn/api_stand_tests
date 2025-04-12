c6858df3c0fb5a637d359a76561e55df2a9e957b
Nalleli Chavez Cortes <nallelichavez87@gmail.com>
2025-04-12 01:59:00 -0500

    changed in def post_new_user

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -5,7 +5,7 @@ import data
 
 
 def post_new_user(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH, json=data.user_body, headers=data.headers)
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH, json=body, headers=data.headers)
 
 response= post_new_user(data.user_body)
 print(response.status_code)
